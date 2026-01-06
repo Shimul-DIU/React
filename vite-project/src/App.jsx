da794aba1e3bab6e1265e50129b725c1cb513b50
MD.Shimul <md.shimuldiu@gmail.com>
2026-01-06 09:02:28 +0600

    add props

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,11 +2,16 @@ import Header from './Components/Header/Header'
 import Footer from './Components/Footer/Footer'
 
 function App() {
- 
+ let student={
+    name:'mustafa',
+    roll:10,
+    cgpa:3.67
+ }
 
   return (
     <>
-      <Header></Header>
+       
+      <Header name='shimul' roll='9' cgpa='3.56' info={student}></Header>
       <Footer></Footer>
     </>
   )
--- vite-project/src/Components/Header/Header.jsx
+++ vite-project/src/Components/Header/Header.jsx
@@ -1,18 +1,23 @@
 import React from "react";
-function Header(){
+function Header(props){
     function myfunction(){
-       document.querySelector('span').innerText=`hello shimul` 
+       document.querySelector('span').style.display='inline-block'
     }
     const styling={
         padding:'10px',
         border: '2px solid black',
         marginTop:'20px',
-        display:'inline-block'
+        display:'none'
     }
     return(
         <div>
-            <span style={styling}>hello</span><br />
+            <span style={styling}>hello i am {props.name} my roll is {props.roll} and cgpa {props.cgpa}</span><br />
            <button onClick={myfunction}>click here</button> <br /><br />
+           <p>my brothe is {props.info.name} her roll {props.info.roll}</p>
+           {Object.entries(props.info).map(([key,value])=>{
+            return<p key={key}>{key}:{value}</p>
+           })}
+           
         </div>
        
     )
